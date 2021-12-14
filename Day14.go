bd34be8870b3ea7371cf49a1d9719bc91beefca0
Jonas Stendel-Cremers <jonas.stendel@valtech.com>
2021-12-14 13:20:05 +0100

    cleanup

--- Day14.go
+++ Day14.go
@@ -22,14 +22,6 @@ func insertOnce(input map[string]int, rules map[string]string) map[string]int {
 	return output
 }
 
-func copyMap(input map[string]int) map[string]int {
-	output := make(map[string]int)
-	for k, v := range input {
-		output[k] = v
-	}
-	return output
-}
-
 func countAndSubtract(input map[string]int) int {
 	most := 0
 	fewest := -1
@@ -56,4 +48,12 @@ func countAndSubtract(input map[string]int) int {
 
 	// % are necessary to care for edges
 	return (most - fewest + ((most % 2) - (fewest % 2))) / 2
+}
+
+func copyMap(input map[string]int) map[string]int {
+	output := make(map[string]int)
+	for k, v := range input {
+		output[k] = v
+	}
+	return output
 }
